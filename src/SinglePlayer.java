5afa5e92dada50a83522abd4e433dd0e4fc3ee46
Narasimha7076 <bablusomesula@gmail.com>
2024-12-12 22:08:46 +0530

    Roll Die

---
+++ src/SinglePlayer.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class SinglePlayer {
+    private int pos = 0;
+
+    public int getPos() {
+        return pos;
+    }
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+
+    public int rollDie(){
+        Random r = new Random();
+        return r.nextInt(6)+1;
+    }
+}
